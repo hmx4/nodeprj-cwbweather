0e4867c2f328b5c9c9135d153b1d0b62433645d3
hmx4 <hungmh1@gmail.com>
2019-04-18 22:08:17 +0800

    update project

--- routes/cwbwq-json.js
+++ routes/cwbwq-json.js
@@ -12,7 +12,7 @@ function cwbwq_json(city, cb)
 	// create the url for querying cwb weather API
 	var baseurl = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?";
 	var AuthCode = "CWB-7B45C9F6-0A6A-48DE-AACF-AEC9D4EDE5E2"; // 請抽換成你的中央氣象局授權碼
-	var citystring =  querystring.stringify('locationName=' + city);
+	var citystring =  querystring.stringify("locationName=" + city);
 	var qstring = citystring + "&elementName=T,RH,WeatherDescription,MaxT,MinT&format=JSON";
 	var cwbweatherurl = baseurl + qstring;
 	//
@@ -34,22 +34,22 @@ function cwbwq_json(city, cb)
 					// 將回傳的JSON字串轉成javascript的JSON物件
 					sdata = JSON.parse(body);
 					// 取得最新一筆平均溫度(攝氏度)
-					temperature = sdata['records']['locations'][0]['location'][0]['weatherElement'][0]['time'][0]['elementValue'][0]['value'];
+					temperature = sdata.records.locations[0].location[0].weatherElement[0].time[0].elementValue[0].value;
 					// 取得最新一筆平均相對溼度
-					humidity = sdata['records']['locations'][0]['location'][0]['weatherElement'][1]['time'][0]['elementValue'][0]['value'];
+					humidity =  sdata.records.locations[0].location[0].weatherElement[1].time[0].elementValue[0].value;
 					// 取的最新一筆天氣預報綜合描述
-					condition = sdata['records']['locations'][0]['location'][0]['weatherElement'][3]['time'][0]['elementValue'][0]['value'];
+					condition = sdata.records.locations[0].location[0].weatherElement[3].time[0].elementValue[0].value;
 					// 取得天氣資訊建立時間
-					time = sdata['records']['locations'][0]['location'][0]['weatherElement'][0]['time'][0]['startTime'];
+					time  =  sdata.records.locations[0].location[0].weatherElement[0].time[0].startTime;
 					//
 					var hightemps=[];
 					var lowtemps=[];
-					// 取得未來一週最高溫度及最低溫度預報值(攝氏12)
+					// 取得未來一週最高溫度及最低溫度預報值(攝氏，間隔12小時)
 					for(i=0; i<=13; i++)
 					{
-						hightemp = sdata['records']['locations'][0]['location'][0]['weatherElement'][4]['time'][i]['elementValue'][0]['value'];
+						hightemp = sdata.records.locations[0].location[0].weatherElement[4].time[i].elementValue[0].value;
 						hightemps.push(hightemp);
-						lowtemp = sdata['records']['locations'][0]['location'][0]['weatherElement'][2]['time'][i]['elementValue'][0]['value'];
+						lowtemp = sdata.records.locations[0].location[0].weatherElement[2].time[i].elementValue[0].value;
 						lowtemps.push(lowtemp);		
 					}
 					// 建立城市簡單天氣資訊之JSON物件
